872184e8abf1e266bc29ae7488acc1afc2392545
pjf <pjf@0ad628ff-c82c-4ca7-9440-a821bc8005a7>
2011-03-22 21:36:39 +0000

    Moved boilerplate M::B subclass code to inc/Bob.pm

---
+++ inc/Bob.pm
@@ -0,0 +1,81 @@
+# @(#)$Id$
+
+package Bob;
+
+use strict;
+use warnings;
+
+use English qw(-no_match_vars);
+
+sub whimper { print {*STDOUT} $_[ 0 ]."\n"; exit 0 }
+
+BEGIN {
+   eval { require 5.008; }; $EVAL_ERROR and whimper 'Perl minimum 5.8';
+   qx(uname -a) =~ m{ profvince.com }mx and whimper 'Stopped vpit';
+   $ENV{PATH}   =~ m{ \A /home/sand }mx and whimper 'Stopped Konig';
+}
+
+use version; our $VERSION = qv( sprintf '0.4.%d', q$Rev$ =~ /\d+/gmx );
+
+use File::Spec::Functions;
+use Module::Build;
+
+sub new {
+   my ($class, $params) = @_;
+
+   my $module     = $params->{module};
+   my $home_page  = $params->{home_page};
+   my $tracker    = $params->{bugtracker};
+   my $distname   = $module; $distname =~ s{ :: }{-}gmx;
+   my $class_path = catfile( q(lib), split m{ :: }mx, $module.q(.pm) );
+   my $resources  = { license => q(http://dev.perl.org/licenses/) };
+
+   $home_page and $resources->{homepage  } = $home_page;
+   $tracker   and $resources->{bugtracker} = $tracker.$distname;
+
+   -f q(MANIFEST.SKIP) and _set_repository( $resources );
+
+   return Module::Build->new
+      ( add_to_cleanup     => [ q(Debian_CPANTS.txt), $distname.q(-*),
+                                map { ( q(*/) x $_ ).q(*~) } 0..5 ],
+        build_requires     => $params->{build_requires},
+        configure_requires => $params->{configure_requires},
+        create_license     => 1,
+        create_packlist    => 0,
+        create_readme      => 1,
+        dist_version_from  => $class_path,
+        license            => $params->{license},
+        meta_merge         => { resources  => $resources, },
+        module_name        => $module,
+        no_index           => { directory  => [ qw(inc t) ], },
+        notes              => { stop_tests => _testing(), },
+        recommends         => $params->{recommends},
+        requires           => $params->{requires},
+        sign               => $params->{sign}, );
+}
+
+# Private subroutines
+
+sub _set_repository {
+   # Accessor for the SVN repository information
+   my $resources = shift;
+
+   require SVN::Class;
+
+   my $file = SVN::Class->svn_dir( q(.) ) or return;
+   my $info = $file->info or return;
+   my $repo = $info->root !~ m{ \A file: }mx ? $info->root : undef;
+
+   $repo and $resources->{repository} = $repo;
+   return;
+}
+
+sub _testing { !! ($ENV{AUTOMATED_TESTING} || $ENV{PERL_CR_SMOKER_CURRENT}
+               || ($ENV{PERL5OPT} || q()) =~ m{ CPAN-Reporter }mx) }
+
+1;
+
+# Local Variables:
+# mode: perl
+# tab-width: 3
+# End:
