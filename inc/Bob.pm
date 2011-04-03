528632480622aaef0e9ccd5ec74666a174ae6f8c
pjf <pjf@0ad628ff-c82c-4ca7-9440-a821bc8005a7>
2011-04-03 14:38:19 +0000

    Tweaked Bob the Module::Builder

--- inc/Bob.pm
+++ inc/Bob.pm
@@ -23,19 +23,11 @@ use Module::Build;
 sub new {
    my ($class, $params) = @_;
 
+   ($params and ref $params eq q(HASH)) or whimper 'No params hash';
+
    my $module     = $params->{module};
-   my $home_page  = $params->{home_page};
-   my $tracker    = defined $params->{bugtracker}
-                  ? $params->{bugtracker}
-                  : q(http://rt.cpan.org/NoAuth/Bugs.html?Dist=);
    my $distname   = $module; $distname =~ s{ :: }{-}gmx;
    my $class_path = catfile( q(lib), split m{ :: }mx, $module.q(.pm) );
-   my $resources  = { license => q(http://dev.perl.org/licenses/) };
-
-   $home_page and $resources->{homepage  } = $home_page;
-   $tracker   and $resources->{bugtracker} = $tracker.$distname;
-
-   -f q(MANIFEST.SKIP) and __set_repository( $resources );
 
    return Module::Build->new
       ( add_to_cleanup     => [ q(Debian_CPANTS.txt), $distname.q(-*),
@@ -47,10 +39,10 @@ sub new {
         create_readme      => 1,
         dist_version_from  => $class_path,
         license            => $params->{license},
-        meta_merge         => { resources  => $resources, },
+        meta_merge         => __get_resources( $params, $distname ),
         module_name        => $module,
-        no_index           => { directory  => [ qw(examples inc t) ], },
-        notes              => __set_notes( $params ),
+        no_index           => __get_no_index( $params ),
+        notes              => __get_notes( $params ),
         recommends         => $params->{recommends},
         requires           => $params->{requires},
         sign               => defined $params->{sign} ? $params->{sign} : 1, );
@@ -58,31 +50,53 @@ sub new {
 
 # Private subroutines
 
-sub __set_notes {
+sub __cpan_testing { !! ($ENV{AUTOMATED_TESTING} || $ENV{PERL_CR_SMOKER_CURRENT}
+                     || ($ENV{PERL5OPT} || q()) =~ m{ CPAN-Reporter }mx) }
+
+sub __get_no_index {
+   my $params = shift;
+
+   return { directory => $params->{no_index_dir} || [ qw(examples inc t) ] };
+}
+
+sub __get_notes {
    my $params = shift; my $notes = $params->{notes} || {};
 
-   $notes->{stop_tests} = $params->{stop_tests} && __testing()
+   $notes->{stop_tests} = $params->{stop_tests} && __cpan_testing()
                         ? 'CPAN Testing stopped' : 0;
 
    return $notes;
 }
 
-sub __set_repository {
+sub __get_repository {
    # Accessor for the SVN repository information
-   my $resources = shift;
-
    require SVN::Class;
 
    my $file = SVN::Class->svn_dir( q(.) ) or return;
    my $info = $file->info or return;
    my $repo = $info->root !~ m{ \A file: }mx ? $info->root : undef;
 
-   $repo and $resources->{repository} = $repo;
-   return;
+   return $repo;
 }
 
-sub __testing { !! ($ENV{AUTOMATED_TESTING} || $ENV{PERL_CR_SMOKER_CURRENT}
-                || ($ENV{PERL5OPT} || q()) =~ m{ CPAN-Reporter }mx) }
+sub __get_resources {
+   my $params     = shift;
+   my $distname   = shift;
+   my $tracker    = defined $params->{bugtracker}
+                  ? $params->{bugtracker}
+                  : q(http://rt.cpan.org/NoAuth/Bugs.html?Dist=);
+   my $resources  = $params->{resources} || {};
+   my $repo;
+
+   $tracker and $resources->{bugtracker} = $tracker.$distname;
+   $params->{home_page} and $resources->{homepage} = $params->{home_page};
+   $resources->{license} ||= q(http://dev.perl.org/licenses/);
+
+   -f q(MANIFEST.SKIP) and $repo = __get_repository
+      and $resources->{repository} = $repo;
+
+   return { resources => $resources };
+}
 
 1;
 
