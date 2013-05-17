9c1a7e39c848f238cdee9bc4eb2f0f4071c59a4b
pjfl <github@roxsoft.co.uk>
2013-05-17 17:05:53 +0100

    0.20.12 Fixed defined/false bug in ::HashMerge

--- lib/File/DataClass.pm
+++ lib/File/DataClass.pm
@@ -1,11 +1,11 @@
-# @(#)$Ident: DataClass.pm 2013-05-16 00:09 pjf ;
+# @(#)$Ident: DataClass.pm 2013-05-17 16:05 pjf ;
 
 package File::DataClass;
 
 use 5.01;
 use strict;
 use namespace::clean -except => 'meta';
-use version; our $VERSION = qv( sprintf '0.20.%d', q$Rev: 10 $ =~ /\d+/gmx );
+use version; our $VERSION = qv( sprintf '0.20.%d', q$Rev: 12 $ =~ /\d+/gmx );
 
 use Moose;
 use MooseX::ClassAttribute;
@@ -32,7 +32,7 @@ File::DataClass - Structured data file IO with OO paradigm
 
 =head1 Version
 
-This document describes version v0.20.$Rev: 10 $ of L<File::DataClass>
+This document describes version v0.20.$Rev: 12 $ of L<File::DataClass>
 
 =head1 Synopsis
 
